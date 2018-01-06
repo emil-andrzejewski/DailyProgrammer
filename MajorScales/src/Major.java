5934569e6ba58848bbb2f3a2ec66a2bd010b2c1f
yarpenzig1 <emil.andrzejewski@gmail.com>
2018-01-06 10:42:10 +0100

    #343 ver2.02
    Usunięcie starych plików

--- MajorScales/src/Major.java
+++
@@ -1,5 +0,0 @@
-
-public class Major {
-	public String major[] = { "do", "re", "mi", "fa", "sol", "la", "ti" };
-	public int nr[] = {0,2,4,5,7,9,11};
-}
--- MajorScales/src/Tony.java
+++
@@ -1,9 +0,0 @@
-
-public class Tony {
-	public String tony[] = { "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B" };
-	public int nr[] = {0,1,2,3,4,5,6,7,8,9,10,11};
-	
-
-	
-	
-}
